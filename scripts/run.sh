7337ce538082c100fa4687dfc49166bd8ea32c4b
Bruno <brunoyui@gmail.com>
2024-11-17 10:07:56 -0200

    adding results description for easy complexity

---
+++ scripts/run.sh
@@ -0,0 +1,26 @@
+# Run all results and generate the graphs
+
+echo "Processing easy results"
+python main.py --lang=EN --metric=EM --sql_complexity=easy --file=../graphs_results/results_easy_EM.png
+python main.py --lang=EN --metric=EX --sql_complexity=easy --file=../graphs_results/results_easy_EX.png
+python main.py --lang=PT --metric=EM --sql_complexity=easy --file=../graphs_results/results_easy_EM_pt.png
+python main.py --lang=PT --metric=EX --sql_complexity=easy --file=../graphs_results/results_easy_EX_pt.png
+echo "Easy results processed"
+
+echo "Processing medium results"
+
+echo "Medium results processed"
+
+
+echo "Processing hard results"
+
+echo "Hard results processed"
+
+
+echo "Processing extra results"
+echo "Extra results processed"
+
+
+echo "Processing no hardness results"
+
+echo "No hardness results processed"
